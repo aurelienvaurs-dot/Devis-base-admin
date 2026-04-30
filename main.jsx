7a363de8096d2f4124f35acddb64dd9c8ef1ac88
aurelienvaurs-dot <aurelien.vaurs@gmail.com>
2026-04-30 23:24:38 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import { applyTheme, getSavedTheme, TK } from "./lib/theme.js";
-import Dashboard from "./pages/Dashboard.jsx";
-
-applyTheme(getSavedTheme() || "dark");
-
-export default function App() {
-  return <Dashboard />;
-}
